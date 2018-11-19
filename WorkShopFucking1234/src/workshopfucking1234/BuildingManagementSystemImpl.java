9f034298af500ef7f77b5e6b0654d169de2fb330
NoobEjby <kasper.ejby@gmail.com>
2018-11-19 18:23:38 +0100

    UPDATIG CLASSES

---
+++ WorkShopFucking1234/src/workshopfucking1234/BuildingManagementSystemImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshopfucking1234;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author Noob
+ */
+public class BuildingManagementSystemImpl {
+    private List buildings;
+    
+    public List<Building> getBuildings(){
+        return buildings;
+    }
+    
+    public void addBuilding(Building b){
+        
+    }
+    public void removeBuilding(UUID id){
+        
+    }
+    
+}
