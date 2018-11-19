63d4412b1ea5f47bebcfdb6f6939eccd63eb95dc
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2018-11-19 17:23:14 +0100

    Add WorkShopFucking1234

---
+++ WorkShopFucking1234/src/workshopfucking1234/WorkShopFucking1234.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshopfucking1234;
+
+/**
+ *
+ * @author jesperisgaard
+ */
+public class WorkShopFucking1234 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
