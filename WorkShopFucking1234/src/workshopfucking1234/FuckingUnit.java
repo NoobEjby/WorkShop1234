0a2f55e7b3aeb4063b1a2bb04558f68b8bb9b39a
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2018-11-19 17:29:28 +0100

    Add FuckingUnit

---
+++ WorkShopFucking1234/src/workshopfucking1234/FuckingUnit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshopfucking1234;
+
+/**
+ *
+ * @author jesperisgaard
+ */
+public abstract class FuckingUnit {
+    String name;
+    
+    @Override
+    public String toString(){
+        return "Something went wrong";
+    }
+}
