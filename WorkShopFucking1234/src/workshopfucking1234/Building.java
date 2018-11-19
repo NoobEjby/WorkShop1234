f843518c38f7b361f41fea314103fb20a601b895
NoobEjby <kasper.ejby@gmail.com>
2018-11-19 17:48:31 +0100

    update

--- WorkShopFucking1234/src/workshopfucking1234/Building.java
+++ WorkShopFucking1234/src/workshopfucking1234/Building.java
@@ -5,10 +5,39 @@
  */
 package workshopfucking1234;
 
+import java.util.List;
+import java.util.UUID;
+
+
 /**
  *
  * @author Noob
  */
 public class Building extends FuckingUnit{
+    private List<Sensor> sensors;
+    private List<Actuator> actuators;
     
+    public List<Sensor> getSensors(){
+        
+        
+    }
+    
+    public List<Actuator> getActuators(){
+        
+        
+    }
+    
+    public void addSensor(Sensor s){
+        
+    }
+    public void removeSensor(UUID id){
+        
+    }
+    
+    public void addActuator(Actuator a){
+        
+    }
+    public void removeActuator(UUID id){
+        
+    }
 }
