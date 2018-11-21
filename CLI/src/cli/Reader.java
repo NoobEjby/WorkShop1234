271670e785d67dc8faa8d6b28330725a1b6f0a37
NoobEjby <kasper.ejby@gmail.com>
2018-11-21 15:09:11 +0100

    skal nok

--- CLI/src/cli/Reader.java
+++ CLI/src/cli/Reader.java
@@ -12,51 +12,86 @@ import java.util.Scanner;
  * @author jesperisgaard
  */
 public class Reader {
+
     Scanner keyboardReader = new Scanner(System.in);
-            System.out.println("The following commands are supported:");
-            System.out.println("\tgetbuildings - ");
-            System.out.println("\taddbuilding - ");
-            System.out.println("\tremovebuilding - ");
-            System.out.println("\tgetbuildinginformation - ");
-            System.out.println("\tgetsensorinformation - ");
-            System.out.println("\tgetactuatorinformation - ");
-            System.out.println("\taddtemperaturesensor - ");
-            System.out.println("\taddco2sensor - ");
-            System.out.println("\tremovesensor - ");
-            System.out.println("\taddventilationactuator - ");
-            System.out.println("\tremoveactuator - ");
-            System.out.println("\tgetbuilding - ");
-            System.out.print(">");
+
+    public Reader() {
+        help();
+    }
+
+    private void play() {
+        boolean running = true;
+
+        while (running)
+        {
+
+        }
+
+    }
+
+    System.out.print (
+    ">");
             String token = keyboardReader.next();
-            if (token.equals("addbuilding")) {
+
+    if (token.equals ( 
+        "addbuilding")) {
                 System.out.println("Which content to write?");
-                System.out.print(">");
+        System.out.print(">");
 
-                String content = keyboardReader.next();
-                
-                Tempo.addBuildings(content);
-            } else if (token.equals("writeend")) {
+        String content = keyboardReader.next();
+
+        Tempo.addBuildings(content);
+    }
+
+    else if (token.equals ( 
+        "writeend")) {
                 System.out.println("Which content to write?");
-                System.out.print(">");
+        System.out.print(">");
 
-                String content = keyboardReader.next();
+        String content = keyboardReader.next();
 
-                cfm.writeAtEnd(content);
-            } else if (token.equals("write")) {
+        cfm.writeAtEnd(content);
+    }
+
+    else if (token.equals ( 
+        "write")) {
                 System.out.println("Which content to write?");
-                System.out.print(">");
-                String content = keyboardReader.next();
+        System.out.print(">");
+        String content = keyboardReader.next();
 
-                System.out.println("Where to write it?");
-                System.out.print(">");
-                
-                int position = keyboardReader.nextInt();
+        System.out.println("Where to write it?");
+        System.out.print(">");
 
-                cfm.write(content, position);
-            } else if (token.equals("exit")) {
+        int position = keyboardReader.nextInt();
+
+        cfm.write(content, position);
+    }
+
+    else if (token.equals ( 
+        "exit")) {
                 System.out.println("Goodbye!");
-                break;
-            } else {
+        break;
+    }
+
+    
+        else {
                 System.out.println("Command not understood!");
-            }
+    }
+
+    private void help() {
+        System.out.println("The following commands are supported:");
+        System.out.println("\tgetbuildings - ");
+        System.out.println("\taddbuilding - ");
+        System.out.println("\tremovebuilding - ");
+        System.out.println("\tgetbuildinginformation - ");
+        System.out.println("\tgetsensorinformation - ");
+        System.out.println("\tgetactuatorinformation - ");
+        System.out.println("\taddtemperaturesensor - ");
+        System.out.println("\taddco2sensor - ");
+        System.out.println("\tremovesensor - ");
+        System.out.println("\taddventilationactuator - ");
+        System.out.println("\tremoveactuator - ");
+        System.out.println("\tgetbuilding - ");
+
+    }
 }
