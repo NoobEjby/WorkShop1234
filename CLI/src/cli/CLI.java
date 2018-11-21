732b374416b36bfcafd7e09cec464f3f9d8a0411
NoobEjby <kasper.ejby@gmail.com>
2018-11-21 15:03:55 +0100

    jaja

--- CLI/src/cli/CLI.java
+++ CLI/src/cli/CLI.java
@@ -1,5 +1,10 @@
 package cli;
 
+import workshopfucking1234.BuildingManagementSystemImpl;
+import workshopfucking1234.IBuildingManagementSystem;
+
+
+
 /**
  *
  * @author Noob
@@ -11,6 +16,8 @@ public class CLI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        IBuildingManagementSystem File = new BuildingManagementSystemImpl();
     }
     
 }
